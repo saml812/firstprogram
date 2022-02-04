48c50813fc498b3cd72e5a7db14d8d8d3b8a7e93
unknown <BT_1E10_16@BKTECH.LOCAL>
2022-02-04 11:14:25 -0500

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Thing thing = new Thing(8);
+        System.out.println(thing.getNum());
+    }
+}
+
+
+
---
+++ src/Thing.java
@@ -0,0 +1,15 @@
+public class Thing {
+    private int num;
+
+    public Thing(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int newNum) {
+        num = newNum;
+    }
+}
